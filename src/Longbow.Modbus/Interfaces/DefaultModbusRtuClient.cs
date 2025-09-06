95ce4056ad428e74ea460025ed040087a06e357b
Argo Zhang <argo@live.ca>
2025-09-06 19:41:15 +0800

    feat: 实现 RTU 读写功能

--- src/Longbow.Modbus/Interfaces/DefaultModbusRtuClient.cs
+++ src/Longbow.Modbus/Interfaces/DefaultModbusRtuClient.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://github.com/LongbowExtensions/
 
+using Longbow.Socket.Algorithm;
 using System.IO.Ports;
 
 namespace Longbow.Modbus;
@@ -77,7 +78,7 @@ class DefaultModbusRtuClient(ModbusRtuClientOptions options) : DefaultModbusClie
 
         // 发送数据
         _readTaskCompletionSource = new TaskCompletionSource();
-        _receiveCancellationTokenSource ??= new();
+        _receiveCancellationTokenSource = new();
         _buffer = ReadOnlyMemory<byte>.Empty;
 
         var request = ModbusRtuMessageBuilder.BuildReadRequest(slaveAddress, functionCode, startAddress, numberOfPoints);
@@ -129,16 +130,19 @@ class DefaultModbusRtuClient(ModbusRtuClientOptions options) : DefaultModbusClie
             throw new InvalidOperationException("站点未连接请先调用 ConnectAsync 方法连接设备");
         }
 
-        var data = ReadOnlyMemory<byte>.Empty;  //WriteBoolValues(address, values);
+        _readTaskCompletionSource = new TaskCompletionSource();
+        _receiveCancellationTokenSource = new();
+        _buffer = ReadOnlyMemory<byte>.Empty;
+
+        var data = WriteBoolValues(address, values);
         var request = ModbusRtuMessageBuilder.BuildWriteRequest(slaveAddress, functionCode, data);
+        _serialPort.DiscardInBuffer();
+        _serialPort.DiscardOutBuffer();
         _serialPort.Write(request.ToArray(), 0, request.Length);
 
-        _readTaskCompletionSource = new TaskCompletionSource();
-        _receiveCancellationTokenSource ??= new();
         await _readTaskCompletionSource.Task.WaitAsync(_receiveCancellationTokenSource.Token);
 
-        var received = ReadOnlyMemory<byte>.Empty;
-        if (!ModbusRtuMessageBuilder.TryValidateReadResponse(received, slaveAddress, functionCode, out var exception))
+        if (!ModbusRtuMessageBuilder.TryValidateWriteResponse(_buffer, functionCode, request, out var exception))
         {
             Exception = exception;
             return false;
@@ -154,22 +158,91 @@ class DefaultModbusRtuClient(ModbusRtuClientOptions options) : DefaultModbusClie
             throw new InvalidOperationException("站点未连接请先调用 ConnectAsync 方法连接设备");
         }
 
-        var data = ReadOnlyMemory<byte>.Empty; // WriteUShortValues(address, values);
+        _readTaskCompletionSource = new TaskCompletionSource();
+        _receiveCancellationTokenSource = new();
+        _buffer = ReadOnlyMemory<byte>.Empty;
+
+        var data = WriteUShortValues(address, values);
         var request = ModbusRtuMessageBuilder.BuildWriteRequest(slaveAddress, functionCode, data);
+        _serialPort.DiscardInBuffer();
+        _serialPort.DiscardOutBuffer();
         _serialPort.Write(request.ToArray(), 0, request.Length);
 
-        _readTaskCompletionSource = new TaskCompletionSource();
-        _receiveCancellationTokenSource ??= new();
         await _readTaskCompletionSource.Task.WaitAsync(_receiveCancellationTokenSource.Token);
 
-        var received = ReadOnlyMemory<byte>.Empty;
-        if (!ModbusRtuMessageBuilder.TryValidateReadResponse(received, slaveAddress, functionCode, out var exception))
+        if (!ModbusRtuMessageBuilder.TryValidateWriteResponse(_buffer, functionCode, request, out var exception))
         {
             Exception = exception;
             return false;
         }
 
         return true;
+    }
+
+    private static ReadOnlyMemory<byte> WriteBoolValues(ushort address, bool[] values)
+    {
+        int byteCount = (values.Length + 7) / 8;
+        var data = new byte[values.Length > 1 ? 5 + byteCount : 4];
+        data[0] = (byte)(address >> 8);
+        data[1] = (byte)address;
+
+        if (values.Length > 1)
+        {
+            // 多值时，写入数量
+            data[2] = (byte)(values.Length >> 8);
+            data[3] = (byte)(values.Length);
+
+            // 字节数
+            data[4] = (byte)(byteCount);
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                if (values[i])
+                {
+                    int byteIndex = 5 + i / 8;
+                    int bitIndex = i % 8;
+                    data[byteIndex] |= (byte)(1 << bitIndex);
+                }
+            }
+        }
+        else
+        {
+            // 组装数据
+            data[2] = values[0] ? (byte)0xFF : (byte)0x00;
+            data[3] = 0x00;
+        }
+
+        return data;
+    }
+
+    private static ReadOnlyMemory<byte> WriteUShortValues(ushort address, ushort[] values)
+    {
+        int byteCount = values.Length * 2;
+        var data = new byte[values.Length > 1 ? 5 + byteCount : 4];
+        data[0] = (byte)(address >> 8);
+        data[1] = (byte)address;
+
+        if (values.Length > 1)
+        {
+            // 多值时，写入数量
+            data[2] = (byte)(values.Length >> 8);
+            data[3] = (byte)(values.Length);
+
+            // 字节数
+            data[4] = (byte)(byteCount);
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                data[i * 2 + 5] = (byte)(values[i] >> 8);
+                data[i * 2 + 6] = (byte)(values[i] & 0xFF);
+            }
+        }
+        else
+        {
+            data[2] = (byte)(values[0] >> 8);
+            data[3] = (byte)(values[0] & 0xFF);
+        }
+        return data;
     }
 
     /// <summary>
