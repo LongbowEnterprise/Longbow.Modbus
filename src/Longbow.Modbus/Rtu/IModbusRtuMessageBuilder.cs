815067e6e30df63b56794c567ef9770943d69a1e
Argo Zhang <argo@live.ca>
2025-09-07 15:29:57 +0800

    refactor: 增加解析编码算法到接口中

--- src/Longbow.Modbus/Rtu/IModbusRtuMessageBuilder.cs
+++ src/Longbow.Modbus/Rtu/IModbusRtuMessageBuilder.cs
@@ -48,4 +48,36 @@ public interface IModbusRtuMessageBuilder
     /// <param name="exception"></param>
     /// <returns></returns>
     bool TryValidateWriteResponse(ReadOnlyMemory<byte> response, byte slaveAddress, byte functionCode, ReadOnlyMemory<byte> data, [NotNullWhen(false)] out Exception? exception);
+
+    /// <summary>
+    /// 解析布尔值数组算法
+    /// </summary>
+    /// <param name="response"></param>
+    /// <param name="numberOfPoints"></param>
+    /// <returns></returns>
+    bool[] ReadBoolValues(ReadOnlyMemory<byte> response, ushort numberOfPoints);
+
+    /// <summary>
+    /// 解析无符号短整数数组算法
+    /// </summary>
+    /// <param name="response"></param>
+    /// <param name="numberOfPoints"></param>
+    /// <returns></returns>
+    ushort[] ReadUShortValues(ReadOnlyMemory<byte> response, ushort numberOfPoints);
+
+    /// <summary>
+    /// 将布尔值数组转换为字节数组算法
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    ReadOnlyMemory<byte> WriteBoolValues(ushort address, bool[] values);
+
+    /// <summary>
+    /// 将无符号短整数数组转换为字节数组算法
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    ReadOnlyMemory<byte> WriteUShortValues(ushort address, ushort[] values);
 }
