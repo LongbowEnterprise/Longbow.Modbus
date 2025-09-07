383f789267a07fdfc19081faf1ad3724aa23357b
Argo Zhang <argo@live.ca>
2025-09-07 15:33:20 +0800

    refactor: 复用解析逻辑

--- src/Longbow.Modbus/RtuOverUdp/DefaultModbusRtuOverUdpClient.cs
+++ src/Longbow.Modbus/RtuOverUdp/DefaultModbusRtuOverUdpClient.cs
@@ -98,36 +98,15 @@ class DefaultModbusRtuOverUpdClient(ModbusUdpClientOptions options, IModbusRtuMe
         return valid ? received : default;
     }
 
-    protected override bool[] ReadBoolValues(ReadOnlyMemory<byte> response, ushort numberOfPoints)
-    {
-        var values = new bool[numberOfPoints];
-        for (var i = 0; i < numberOfPoints; i++)
-        {
-            var byteIndex = 3 + i / 8;
-            var bitIndex = i % 8;
-            values[i] = (response.Span[byteIndex] & (1 << bitIndex)) != 0;
-        }
+    protected override bool[] ReadBoolValues(ReadOnlyMemory<byte> response, ushort numberOfPoints) => builder.ReadBoolValues(response, numberOfPoints);
 
-        return values;
-    }
-
-    protected override ushort[] ReadUShortValues(ReadOnlyMemory<byte> response, ushort numberOfPoints)
-    {
-        var values = new ushort[numberOfPoints];
-        for (var i = 0; i < numberOfPoints; i++)
-        {
-            int offset = 3 + (i * 2);
-            values[i] = (ushort)((response.Span[offset] << 8) | response.Span[offset + 1]);
-        }
-
-        return values;
-    }
+    protected override ushort[] ReadUShortValues(ReadOnlyMemory<byte> response, ushort numberOfPoints) => builder.ReadUShortValues(response, numberOfPoints);
 
     protected override async ValueTask<bool> WriteBoolValuesAsync(byte slaveAddress, byte functionCode, ushort address, bool[] values)
     {
         _client.ThrowIfNotConnected();
 
-        var data = WriteBoolValues(address, values);
+        var data = builder.WriteBoolValues(address, values);
         var request = builder.BuildWriteRequest(slaveAddress, functionCode, data);
         await SendAsync(request);
         var received = await ReceiveAsync();
@@ -146,7 +125,7 @@ class DefaultModbusRtuOverUpdClient(ModbusUdpClientOptions options, IModbusRtuMe
     {
         _client.ThrowIfNotConnected();
 
-        var data = WriteUShortValues(address, values);
+        var data = builder.WriteUShortValues(address, values);
         var request = builder.BuildWriteRequest(slaveAddress, functionCode, data);
         await SendAsync(request);
         var response = await ReceiveAsync();
@@ -159,73 +138,6 @@ class DefaultModbusRtuOverUpdClient(ModbusUdpClientOptions options, IModbusRtuMe
         var valid = builder.TryValidateWriteResponse(response, slaveAddress, functionCode, data, out var exception);
         Exception = valid ? null : exception;
         return valid;
-    }
-
-    private static ReadOnlyMemory<byte> WriteBoolValues(ushort address, bool[] values)
-    {
-        int byteCount = (values.Length + 7) / 8;
-        var data = new byte[values.Length > 1 ? 5 + byteCount : 4];
-        data[0] = (byte)(address >> 8);
-        data[1] = (byte)address;
-
-        if (values.Length > 1)
-        {
-            // 多值时，写入数量
-            data[2] = (byte)(values.Length >> 8);
-            data[3] = (byte)(values.Length);
-
-            // 字节数
-            data[4] = (byte)(byteCount);
-
-            for (var i = 0; i < values.Length; i++)
-            {
-                if (values[i])
-                {
-                    int byteIndex = 5 + i / 8;
-                    int bitIndex = i % 8;
-                    data[byteIndex] |= (byte)(1 << bitIndex);
-                }
-            }
-        }
-        else
-        {
-            // 组装数据
-            data[2] = values[0] ? (byte)0xFF : (byte)0x00;
-            data[3] = 0x00;
-        }
-
-        return data;
-    }
-
-    private static ReadOnlyMemory<byte> WriteUShortValues(ushort address, ushort[] values)
-    {
-        int byteCount = values.Length * 2;
-        var data = new byte[values.Length > 1 ? 5 + byteCount : 4];
-        data[0] = (byte)(address >> 8);
-        data[1] = (byte)address;
-
-        if (values.Length > 1)
-        {
-            // 多值时，写入数量
-            data[2] = (byte)(values.Length >> 8);
-            data[3] = (byte)(values.Length);
-
-            // 字节数
-            data[4] = (byte)(byteCount);
-
-            for (var i = 0; i < values.Length; i++)
-            {
-                data[i * 2 + 5] = (byte)(values[i] >> 8);
-                data[i * 2 + 6] = (byte)(values[i] & 0xFF);
-            }
-        }
-        else
-        {
-            data[2] = (byte)(values[0] >> 8);
-            data[3] = (byte)(values[0] & 0xFF);
-        }
-
-        return data;
     }
 
     /// <summary>
