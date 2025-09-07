ff1f26e3245e1b7423997c29a2d14284e1a9000a
Argo Zhang <argo@live.ca>
2025-09-07 14:57:52 +0800

    test: 更新单元测试

--- test/UnitTestModbus/Rtu/RtuOverTcpClientTest.cs
+++ test/UnitTestModbus/Rtu/RtuOverTcpClientTest.cs
@@ -14,14 +14,15 @@ public class RtuOverTcpClientTest
     public async Task ReadCoilsAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        var connected = await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         Assert.True(connected);
 
         var response = await client.ReadCoilsAsync(0x01, 0, 10);
@@ -33,14 +34,15 @@ public class RtuOverTcpClientTest
     public async Task ReadInputsAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.ReadInputsAsync(0x01, 0, 10);
         Assert.NotNull(response);
         Assert.Equal(10, response.Length);
@@ -50,14 +52,15 @@ public class RtuOverTcpClientTest
     public async Task ReadHoldingRegistersAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.ReadHoldingRegistersAsync(0x01, 0, 10);
         Assert.NotNull(response);
         Assert.Equal(10, response.Length);
@@ -67,14 +70,15 @@ public class RtuOverTcpClientTest
     public async Task ReadInputRegistersAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.ReadInputRegistersAsync(0x01, 0, 10);
         Assert.NotNull(response);
         Assert.Equal(10, response.Length);
@@ -84,14 +88,15 @@ public class RtuOverTcpClientTest
     public async Task WriteCoilAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.WriteCoilAsync(0x01, 0, true);
         Assert.True(response);
 
@@ -103,14 +108,15 @@ public class RtuOverTcpClientTest
     public async Task WriteMultipleCoilsAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.WriteMultipleCoilsAsync(0x01, 0, [true, true, true, true, true, true, true, true, false, true]);
         Assert.True(response);
     }
@@ -119,14 +125,15 @@ public class RtuOverTcpClientTest
     public async Task WriteRegisterAsync()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.WriteRegisterAsync(0x01, 0, 12);
         Assert.True(response);
     }
@@ -135,14 +142,15 @@ public class RtuOverTcpClientTest
     public async Task WriteMultipleRegistersAsync_Ok()
     {
         var sc = new ServiceCollection();
+        sc.AddTcpSocketFactory();
         sc.AddModbusFactory();
 
         var provider = sc.BuildServiceProvider();
         var factory = provider.GetRequiredService<IModbusFactory>();
-        await using var client = factory.GetOrCreateRtuMaster("test");
+        var client = factory.GetOrCreateRtuOverTcpMaster("test");
 
         // 连接 Master
-        await client.ConnectAsync();
+        var connected = await client.ConnectAsync("127.0.0.1", 502);
         var response = await client.WriteMultipleRegistersAsync(0x01, 0, [12, 0, 23, 0, 46, 0, 01, 02, 04, 05]);
         Assert.True(response);
     }
