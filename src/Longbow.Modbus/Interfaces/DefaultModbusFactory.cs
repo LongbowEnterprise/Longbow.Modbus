a9bfebde6c36d4c2e6441e84baafb753eafd09a3
Argo Zhang <argo@live.ca>
2025-09-16 16:34:32 +0800

    refactor: 重构 IModbusFactory 接口

--- src/Longbow.Modbus/Interfaces/DefaultModbusFactory.cs
+++ src/Longbow.Modbus/Interfaces/DefaultModbusFactory.cs
@@ -4,6 +4,7 @@
 
 using Longbow.SerialPorts;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System.Collections.Concurrent;
 
 namespace Longbow.Modbus;
@@ -19,18 +20,20 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
     private readonly ConcurrentDictionary<string, IModbusTcpClient> _rtuOverTcpPool = new();
     private readonly ConcurrentDictionary<string, IModbusTcpClient> _rtuOverUdpPool = new();
 
-    public IModbusTcpClient GetOrCreateTcpMaster(string? name, Action<ModbusTcpClientOptions>? valueFactory = null) => string.IsNullOrEmpty(name)
-        ? CreateTcpClient(valueFactory)
-        : _tcpPool.GetOrAdd(name, key => CreateTcpClient(valueFactory));
+    public IModbusTcpClient GetOrCreateTcpMaster(string name, Action<ModbusTcpClientOptions>? configureOptions = null) => string.IsNullOrEmpty(name)
+        ? CreateTcpClient(configureOptions)
+        : _tcpPool.GetOrAdd(name, key => CreateTcpClient(configureOptions));
 
-    private DefaultTcpClient CreateTcpClient(Action<ModbusTcpClientOptions>? valueFactory = null)
+    public IModbusTcpClient GetOrCreateTcpMaster(Action<ModbusTcpClientOptions>? configureOptions = null) => CreateTcpClient(configureOptions);
+
+    private DefaultTcpClient CreateTcpClient(Action<ModbusTcpClientOptions>? configureOptions = null)
     {
         var factory = provider.GetRequiredService<ITcpSocketFactory>();
 
         var options = new ModbusTcpClientOptions();
-        valueFactory?.Invoke(options);
+        configureOptions?.Invoke(options);
 
-        var client = factory.GetOrCreate(valueFactory: op =>
+        var client = factory.GetOrCreate(configureOptions: op =>
         {
             op.ConnectTimeout = options.ConnectTimeout;
             op.SendTimeout = options.WriteTimeout;
@@ -55,19 +58,20 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
         return client;
     }
 
-    public IModbusRtuClient GetOrCreateRtuMaster(string? name = null, Action<ModbusRtuClientOptions>? valueFactory = null) => string.IsNullOrEmpty(name)
-        ? CreateRtuClient(valueFactory)
-        : _rtuPool.GetOrAdd(name, key => CreateRtuClient(valueFactory));
+    public IModbusRtuClient GetOrCreateRtuMaster(string name, Action<ModbusRtuClientOptions>? configureOptions = null) => string.IsNullOrEmpty(name)
+        ? CreateRtuClient(configureOptions)
+        : _rtuPool.GetOrAdd(name, key => CreateRtuClient(configureOptions));
 
+    public IModbusRtuClient GetOrCreateRtuMaster(Action<ModbusRtuClientOptions>? configureOptions = null) => CreateRtuClient(configureOptions);
 
-    private DefaultRtuClient CreateRtuClient(Action<ModbusRtuClientOptions>? valueFactory = null)
+    private DefaultRtuClient CreateRtuClient(Action<ModbusRtuClientOptions>? configureOptions = null)
     {
         var factory = provider.GetRequiredService<ISerialPortFactory>();
 
         var options = new ModbusRtuClientOptions();
-        valueFactory?.Invoke(options);
+        configureOptions?.Invoke(options);
 
-        var client = factory.GetOrCreate(valueFactory: options.ToSerialPortOptions);
+        var client = factory.GetOrCreate(options.ToSerialPortOptions);
         var builder = provider.GetRequiredService<IModbusRtuMessageBuilder>();
         return new DefaultRtuClient(client, builder);
     }
@@ -82,12 +86,12 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
         return client;
     }
 
-    public IModbusTcpClient GetOrCreateUdpMaster(string? name = null, Action<ModbusUdpClientOptions>? valueFactory = null)
+    public IModbusTcpClient GetOrCreateUdpMaster(string name, Action<ModbusUdpClientOptions>? configureOptions = null)
     {
         if (string.IsNullOrEmpty(name))
         {
             var options = new ModbusUdpClientOptions();
-            valueFactory?.Invoke(options);
+            configureOptions?.Invoke(options);
             return new DefaultUdpClient(options, provider.GetRequiredService<IModbusTcpMessageBuilder>());
         }
         if (_udpPool.TryGetValue(name, out var client))
@@ -97,10 +101,17 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
 
 
         var op = new ModbusUdpClientOptions();
-        valueFactory?.Invoke(op);
+        configureOptions?.Invoke(op);
         client = new DefaultUdpClient(op, provider.GetRequiredService<IModbusTcpMessageBuilder>());
         _udpPool.TryAdd(name, client);
         return client;
+    }
+
+    public IModbusTcpClient GetOrCreateUdpMaster(Action<ModbusUdpClientOptions>? configureOptions = null)
+    {
+        var op = new ModbusUdpClientOptions();
+        configureOptions?.Invoke(op);
+        return new DefaultUdpClient(op, provider.GetRequiredService<IModbusTcpMessageBuilder>());
     }
 
     public IModbusTcpClient? RemoveUdpMaster(string name)
@@ -113,18 +124,20 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
         return client;
     }
 
-    public IModbusTcpClient GetOrCreateRtuOverTcpMaster(string? name = null, Action<ModbusTcpClientOptions>? valueFactory = null) => string.IsNullOrEmpty(name)
-        ? CreateRtuOverTcpClient(valueFactory)
-        : _rtuOverTcpPool.GetOrAdd(name, key => CreateRtuOverTcpClient(valueFactory));
+    public IModbusTcpClient GetOrCreateRtuOverTcpMaster(string name, Action<ModbusTcpClientOptions>? configureOptions = null) => string.IsNullOrEmpty(name)
+        ? CreateRtuOverTcpClient(configureOptions)
+        : _rtuOverTcpPool.GetOrAdd(name, key => CreateRtuOverTcpClient(configureOptions));
 
-    private DefaultRtuOverTcpClient CreateRtuOverTcpClient(Action<ModbusTcpClientOptions>? valueFactory = null)
+    public IModbusTcpClient GetOrCreateRtuOverTcpMaster(Action<ModbusTcpClientOptions>? valueFactory = null) => CreateRtuOverTcpClient(valueFactory);
+
+    private DefaultRtuOverTcpClient CreateRtuOverTcpClient(Action<ModbusTcpClientOptions>? configureOptions = null)
     {
         var factory = provider.GetRequiredService<ITcpSocketFactory>();
 
         var options = new ModbusTcpClientOptions();
-        valueFactory?.Invoke(options);
+        configureOptions?.Invoke(options);
 
-        var client = factory.GetOrCreate(valueFactory: op =>
+        var client = factory.GetOrCreate(configureOptions: op =>
         {
             op.ConnectTimeout = options.ConnectTimeout;
             op.SendTimeout = options.WriteTimeout;
@@ -148,12 +161,12 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
         return client;
     }
 
-    public IModbusTcpClient GetOrCreateRtuOverUdpMaster(string? name = null, Action<ModbusUdpClientOptions>? valueFactory = null)
+    public IModbusTcpClient GetOrCreateRtuOverUdpMaster(string name, Action<ModbusUdpClientOptions>? configureOptions = null)
     {
         if (string.IsNullOrEmpty(name))
         {
             var options = new ModbusUdpClientOptions();
-            valueFactory?.Invoke(options);
+            configureOptions?.Invoke(options);
             return new DefaultModbusRtuOverUpdClient(options, provider.GetRequiredService<IModbusRtuMessageBuilder>());
         }
         if (_rtuOverUdpPool.TryGetValue(name, out var client))
@@ -163,10 +176,17 @@ class DefaultModbusFactory(IServiceProvider provider) : IModbusFactory
 
 
         var op = new ModbusUdpClientOptions();
-        valueFactory?.Invoke(op);
+        configureOptions?.Invoke(op);
         client = new DefaultModbusRtuOverUpdClient(op, provider.GetRequiredService<IModbusRtuMessageBuilder>());
         _rtuOverUdpPool.TryAdd(name, client);
         return client;
+    }
+
+    public IModbusTcpClient GetOrCreateRtuOverUdpMaster(Action<ModbusUdpClientOptions>? configureOptions = null)
+    {
+        var op = new ModbusUdpClientOptions();
+        configureOptions?.Invoke(op);
+        return new DefaultModbusRtuOverUpdClient(op, provider.GetRequiredService<IModbusRtuMessageBuilder>());
     }
 
     public IModbusTcpClient? RemoveRtuOverUdpMaster(string name)
