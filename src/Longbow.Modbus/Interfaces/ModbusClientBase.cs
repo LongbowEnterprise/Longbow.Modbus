989a8dba57cd1bbdbc0a9854d113e1a67cfc0a33
Argo Zhang <argo@live.ca>
2025-09-17 18:24:08 +0800

    feat: 增加参数合规检查逻辑

--- src/Longbow.Modbus/Interfaces/ModbusClientBase.cs
+++ src/Longbow.Modbus/Interfaces/ModbusClientBase.cs
@@ -14,24 +14,32 @@ abstract class ModbusClientBase(IModbusMessageBuilder builder) : IModbusClient
 
     public async ValueTask<bool[]> ReadCoilsAsync(byte slaveAddress, ushort startAddress, ushort numberOfPoints, CancellationToken token = default)
     {
+        MessageBuilder.ValidateNumberOfPoints(nameof(numberOfPoints), numberOfPoints, 2000);
+
         var response = await ReadAsync(slaveAddress, 0x01, startAddress, numberOfPoints, token);
         return builder.ReadBoolValues(response, numberOfPoints);
     }
 
-    public async ValueTask<bool[]> ReadInputsAsync(byte slaveAddress, ushort startAddress, ushort numberOfInputs, CancellationToken token = default)
+    public async ValueTask<bool[]> ReadInputsAsync(byte slaveAddress, ushort startAddress, ushort numberOfPoints, CancellationToken token = default)
     {
-        var response = await ReadAsync(slaveAddress, 0x02, startAddress, numberOfInputs, token);
-        return builder.ReadBoolValues(response, numberOfInputs);
+        MessageBuilder.ValidateNumberOfPoints(nameof(numberOfPoints), numberOfPoints, 2000);
+
+        var response = await ReadAsync(slaveAddress, 0x02, startAddress, numberOfPoints, token);
+        return builder.ReadBoolValues(response, numberOfPoints);
     }
 
     public async ValueTask<ushort[]> ReadHoldingRegistersAsync(byte slaveAddress, ushort startAddress, ushort numberOfPoints, CancellationToken token = default)
     {
+        MessageBuilder.ValidateNumberOfPoints(nameof(numberOfPoints), numberOfPoints, 125);
+
         var response = await ReadAsync(slaveAddress, 0x03, startAddress, numberOfPoints, token);
         return builder.ReadUShortValues(response, numberOfPoints);
     }
 
     public async ValueTask<ushort[]> ReadInputRegistersAsync(byte slaveAddress, ushort startAddress, ushort numberOfPoints, CancellationToken token = default)
     {
+        MessageBuilder.ValidateNumberOfPoints(nameof(numberOfPoints), numberOfPoints, 125);
+
         var response = await ReadAsync(slaveAddress, 0x04, startAddress, numberOfPoints, token);
         return builder.ReadUShortValues(response, numberOfPoints);
     }
@@ -40,9 +48,19 @@ abstract class ModbusClientBase(IModbusMessageBuilder builder) : IModbusClient
 
     public ValueTask<bool> WriteRegisterAsync(byte slaveAddress, ushort registerAddress, ushort value, CancellationToken token = default) => WriteUShortValuesAsync(slaveAddress, 0x06, registerAddress, [value], token);
 
-    public ValueTask<bool> WriteMultipleCoilsAsync(byte slaveAddress, ushort startAddress, bool[] values, CancellationToken token = default) => WriteBoolValuesAsync(slaveAddress, 0x0F, startAddress, values, token);
+    public ValueTask<bool> WriteMultipleCoilsAsync(byte slaveAddress, ushort startAddress, bool[] values, CancellationToken token = default)
+    {
+        MessageBuilder.ValidateData(nameof(values), values, 1968);
 
-    public ValueTask<bool> WriteMultipleRegistersAsync(byte slaveAddress, ushort registerAddress, ushort[] values, CancellationToken token = default) => WriteUShortValuesAsync(slaveAddress, 0x10, registerAddress, values, token);
+        return WriteBoolValuesAsync(slaveAddress, 0x0F, startAddress, values, token);
+    }
+
+    public ValueTask<bool> WriteMultipleRegistersAsync(byte slaveAddress, ushort registerAddress, ushort[] values, CancellationToken token = default)
+    {
+        MessageBuilder.ValidateData(nameof(values), values, 123);
+
+        return WriteUShortValuesAsync(slaveAddress, 0x10, registerAddress, values, token);
+    }
 
     private async ValueTask<ReadOnlyMemory<byte>> ReadAsync(byte slaveAddress, byte functionCode, ushort startAddress, ushort numberOfPoints, CancellationToken token = default)
     {
---
+++ src/Longbow.Modbus/Utility/MessageBuilder.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+namespace Longbow.Modbus;
+
+static class MessageBuilder
+{
+    public static void ValidateNumberOfPoints(string argumentName, ushort numberOfPoints, ushort maxNumberOfPoints)
+    {
+        if (numberOfPoints < 1 || numberOfPoints > maxNumberOfPoints)
+        {
+            string msg = $"Argument {argumentName} must be between 1 and {maxNumberOfPoints} inclusive.";
+            throw new ArgumentException(msg);
+        }
+    }
+
+    public static void ValidateData<T>(string argumentName, T[] data, int maxDataLength)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+
+        if (data.Length == 0 || data.Length > maxDataLength)
+        {
+            string msg = $"The length of argument {argumentName} must be between 1 and {maxDataLength} inclusive.";
+            throw new ArgumentException(msg);
+        }
+    }
+}
