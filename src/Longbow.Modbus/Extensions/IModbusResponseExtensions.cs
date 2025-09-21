569340cd7534db741221537f49602686d80ce494
Argo Zhang <argo@live.ca>
2025-09-21 10:41:45 +0800

    feat: 增加 IModbusResponse 扩展方法

---
+++ src/Longbow.Modbus/Extensions/IModbusResponseExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+namespace Longbow.Modbus;
+
+/// <summary>
+/// <see cref="IModbusResponse"/> 扩展方法"
+/// </summary>
+public static class IModbusResponseExtensions
+{
+    /// <summary>
+    /// 将 <see cref="IModbusResponse"/> 实例中 <see cref="IModbusResponse.RawData"/> 转换成布尔数组
+    /// </summary>
+    /// <param name="response"></param>
+    /// <param name="numberOfPoints"></param>
+    /// <returns></returns>
+    public static bool[] ReadBoolValues(this IModbusResponse response, ushort numberOfPoints) => response.Builder.ReadBoolValues(response.RawData, numberOfPoints);
+
+    /// <summary>
+    /// 将 <see cref="IModbusResponse"/> 实例中 <see cref="IModbusResponse.RawData"/> 转换成无符号短整型数组
+    /// </summary>
+    /// <param name="response"></param>
+    /// <param name="numberOfPoints"></param>
+    /// <returns></returns>
+    public static ushort[] ReadUShortValues(this IModbusResponse response, ushort numberOfPoints) => response.Builder.ReadUShortValues(response.RawData, numberOfPoints);
+}
