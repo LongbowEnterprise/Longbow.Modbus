b160ebd7747ec478e672845dae71bd0af5509b2d
Argo Zhang <argo@live.ca>
2025-09-21 10:34:20 +0800

    refactor: 增加 IModbusResponse 接口

---
+++ src/Longbow.Modbus/Interfaces/IModbusResponse.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://github.com/LongbowExtensions/
+
+namespace Longbow.Modbus;
+
+/// <summary>
+/// Modbus 返回数据接口
+/// </summary>
+public interface IModbusResponse
+{
+    /// <summary>
+    /// 获得 原始数据
+    /// </summary>
+    ReadOnlyMemory<byte> RawData { get; }
+}
